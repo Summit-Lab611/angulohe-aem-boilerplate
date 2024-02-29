a55b0e1770604ee5feb3a471f37ac0501f793429
summitlab611u00 <161488441+summitlab611u00@users.noreply.github.com>
2024-02-29 15:50:44 +0100

    Update quote.js

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,10 +1,10 @@
 export default async function decorate(block) {
-    block.innerHTML = `
-        <div class=quote-quotation">
-            ${block.firstElementChild.firstElementChild.innerText}
-        </div>
-        <div class="quote-attribution">
-            ${block.firstElementChild.lastElementChild.innerText}
-        </div>
-    `;
+  block.innerHTML = `
+    <div class=quote-quotation">
+        ${block.firstElementChild.firstElementChild.innerText}
+    </div>
+    <div class="quote-attribution">
+        ${block.firstElementChild.lastElementChild.innerText}
+    </div>
+  `;
 }
